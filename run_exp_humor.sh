274f36ccc9c4ddf390f9a5754bfd22182036e081
lc0197 <44441963+lc0197@users.noreply.github.com>
2022-05-10 12:41:33 +0200

    Updated run_exp_humor with bert run

--- run_exp_humor.sh
+++ run_exp_humor.sh
@@ -13,3 +13,7 @@ python3 main.py --task humor --d_rnn 256 --rnn_bi --rnn_n_layers 4 --lr 0.0001 -
 # VGGFACE
 
 python3 main.py --task humor --d_rnn 64 --rnn_bi --rnn_n_layers 2 --lr 0.0001 --feature vggface2 --cache --result_csv  baseline_results/humor.csv --n_seeds 5 --seed 101 --batch_size 256  --early_stopping_patience 3 --reduce_lr_patience 2
+
+# BERT
+
+python3 main.py --task humor --d_rnn 128 --rnn_bi --rnn_n_layers 4 --lr 0.001 --feature bert-4-sentence-level --cache --result_csv  baseline_results/humor.csv --n_seeds 5 --seed 101 --batch_size 256  --early_stopping_patience 3 --reduce_lr_patience 2
